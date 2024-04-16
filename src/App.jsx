9c7791ca4d5b6fc4d87ad1ef295397fbbc04f302
gokuthecoder <vishalkr12012001@gmail.com>
2024-04-16 20:09:04 +0530

    styling updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <div>
       <div className="mb-4 pb-16">
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-white">
-      <span><img src="goku.svg" alt="" width="100px" className="block mx-auto "/></span><span id="main__title">The People of the Kingdom</span>
+      <h2 className="scroll-m-19 border-b-2 pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-white inline-block border-b-fuchsia-400 rounded">
+      <span><img src="goku.svg" alt="" width="100px" className="block mx-auto "/></span><span id="main__title">Our Testimonial </span>
     </h2>
       </div>
       <Testimonial reviews={reviews} />
